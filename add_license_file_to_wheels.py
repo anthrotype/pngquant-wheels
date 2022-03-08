63bcd3dcb10ce03d9fee572b26d3bf8fd80d8675
Cosimo Lupo <clupo@google.com>
2022-03-08 11:33:05 +0000

    add_license_file_to_wheels.py: print name of wheel

--- add_license_file_to_wheels.py
+++ add_license_file_to_wheels.py
@@ -15,6 +15,7 @@ def main():
     # maturin has problems adding the license text to the wheel archive yet so we
     # have to do it ourselves: cf. https://github.com/PyO3/maturin/issues/829
     for wheel_file in sys.argv[1:]:
+        print(f"Adding '{license.name}' to '{wheel_file}'")
         with InWheel(in_wheel=wheel_file, out_wheel=wheel_file) as tmpdir:
             distinfo = next(Path(tmpdir).glob("*.dist-info"))
             assert distinfo.is_dir()
