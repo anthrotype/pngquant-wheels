67f1b951c8f7df94bc4d3c59e5e6afa6b72c3644
Cosimo Lupo <clupo@google.com>
2024-10-02 10:26:50 +0100

    update to latest pngquant 3.0.3, use latest maturin
    Brings pngquant-cli up to date with latest library and toolchain, similar to changes applied in resvg-wheels repo.

--- README.md
+++
@@ -1,5 +0,0 @@
-![CI Status](https://github.com/anthrotype/pngquant-wheels/actions/workflows/ci.yml/badge.svg)
-
-# pngquant wheels
-
-This repository is used to build and publish precompiled Python [wheels](https://pythonwheels.com/) for [pngquant](https://github.com/kornelski/pngquant), using [maturin](https://maturin.rs/).
--- build.py
+++ build.py
@@ -1,44 +1,30 @@
 #!/usr/bin/env python3
 
-import tomli
-import tomli_w
-import shutil
 import subprocess
 import sys
 from pathlib import Path
 
 
 root_dir = Path(__file__).parent.resolve()
-crate_dir = root_dir / "pngquant"
 dist_dir = root_dir / "dist"
-cargo_manifest_path = crate_dir / "Cargo.toml"
-pyproject_toml_path = root_dir / "pyproject.toml"
 
 
 def main():
-    # maturin expects the 'pyproject.toml' file to be included in the cargo
-    # package and located in the same directory as Cargo.toml, so we add it
-    # to the package's 'include' list and copy it there.
-    cargo_manifest = tomli.load(cargo_manifest_path.open("rb"))
-    if (
-        "include" in cargo_manifest["package"]
-        and "/pyproject.toml" not in cargo_manifest["package"]["include"]
-    ):
-        cargo_manifest["package"]["include"].append("/pyproject.toml")
-        tomli_w.dump(cargo_manifest, cargo_manifest_path.open("wb"))
-
-    shutil.copyfile(pyproject_toml_path, crate_dir / "pyproject.toml")
-
     try:
         cmd = {
             "sdist": ["maturin", "sdist"],
-            "wheel": ["maturin", "build", "--no-sdist", "--release"],
+            "wheel": ["maturin", "build", "--release"],
         }[sys.argv[1]]
     except (IndexError, KeyError):
         sys.exit("usage: build.py [sdist|wheel]")
 
+    args = sys.argv[2:]
+    if "--universal2" in args:
+        cmd.extend(["--target", "universal2-apple-darwin"])
+        args.remove("--universal2")
+
     return subprocess.call(
-        cmd + ["-o", str(dist_dir)] + sys.argv[2:], cwd=str(crate_dir)
+        cmd + ["-o", str(dist_dir)] + args
     )
 
 
